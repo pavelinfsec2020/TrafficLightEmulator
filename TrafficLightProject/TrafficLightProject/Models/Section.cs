103768b407c6ce2cad4d0961f5f860c8f8587551
infsec <infsec@HOME-PC>
2025-01-06 16:38:21 +0300

    feat: Доработана привязка binding для свойства включения и выключения секции светофора через паттерн MVVM. Описана модель светофора, добавлено поведение режима NonStop

--- TrafficLightProject/TrafficLightProject/Models/Section.cs
+++ TrafficLightProject/TrafficLightProject/Models/Section.cs
@@ -1,27 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TrafficLightProject.Models
 {
-    internal abstract class Section
+    internal abstract class Section : INotifyPropertyChanged
     {
-
+        private bool _isEnabled;
         public Section(int timeInterval)
         { 
-           TimeInterval = timeInterval;
+           TimeIntervalSeconds = timeInterval;
         }
         public Color SectionColor { get; protected set; }
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled { get { return _isEnabled; } set { _isEnabled = value; OnPropertyChanged(nameof(IsEnabled)); } }
 
-        public int TimeInterval { get; private set; }
+        public int TimeIntervalSeconds { get; set; }
         public TrafficLightPosition Position { get; protected set; }
 
         protected virtual TrafficLightPosition GetPosition()
         { 
            return TrafficLightPosition.None;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- TrafficLightProject/TrafficLightProject/Models/TrafficLight.cs
+++ TrafficLightProject/TrafficLightProject/Models/TrafficLight.cs
@@ -8,5 +8,77 @@ namespace TrafficLightProject.Models
 {
     internal class TrafficLight
     {
+        public TrafficLight()
+        {
+            TurnSections = new List<TurnSection>();
+            MainSections = new MainSection[]
+            {
+                new MainSection (Color.Red, 5),
+                new MainSection (Color.Yellow, 1),
+                new MainSection (Color.Green, 10),
+            };
+        }
+        public bool IsActivated { get; set; }
+        public MainSection[] MainSections { get; set; }
+        public List<TurnSection> TurnSections { get; set; }
+
+        public bool AddTurnSection(ArrowTurn arrow, int interval)
+        {
+            if (TurnSections.Any(s => s.ArrowTurn == arrow))
+                return false;
+
+            TurnSections.Add(new TurnSection(interval, arrow));
+
+            return true;
+        }
+
+        public bool RemoveTurnSection(ArrowTurn arrow)
+        {
+            if (TurnSections.Any(s => s.ArrowTurn != arrow))
+                return false;
+
+            TurnSections.Remove(
+                TurnSections.FirstOrDefault(s => s.ArrowTurn == arrow));
+
+            return true;
+        }
+
+        public void EnableTimerDisplay()
+        {
+            MainSections[0].HasTimerDisplay = true;
+            MainSections[2].HasTimerDisplay = true;
+        }
+
+        public void DisableTimerDisplay()
+        {
+            MainSections[0].HasTimerDisplay = false;
+            MainSections[2].HasTimerDisplay = false;
+        }
+
+        public async void ActivateNonStopMode()
+        {
+            TurnOffTrafficLight();
+           
+            while (IsActivated)
+            {
+                var delay = MainSections[1].TimeIntervalSeconds * 1000;
+                MainSections[1].IsEnabled = true;
+                await Task.Delay(delay);
+                MainSections[1].IsEnabled = false;
+                await Task.Delay(delay);
+            }
+        }
+
+        public void TurnOffTrafficLight()
+        { 
+           foreach (var section in MainSections)
+                section.IsEnabled = false;
+
+            if (TurnSections.Any())
+            {
+                foreach (var section in TurnSections)
+                    section.IsEnabled = false;
+            }
+        }
     }
 }
